746151fe8147833e84e51a82087517abb4673882
Aaron <erinadost@gmail.com>
2024-03-10 14:03:11 -0400

    update dark styling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -20,17 +20,17 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-gray-800 dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto p-6 border  ">
+      <CardBody className="relative group/card  hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-gray-800 border-white/[0.2] w-auto sm:w-[30rem] h-auto p-6 border  ">
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          className="text-xl font-bold text-white"
         >
           {title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          className="text-sm max-w-sm mt-2 text-neutral-300"
         >
           {description}
         </CardItem>
@@ -45,12 +45,12 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
         </CardItem>
         <div className="flex justify-between items-center mt-20">
           <Link href={viewDetailsLink}>
-            <button className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white">
+            <button className="px-4 py-2 rounded-xl text-xs font-normaltext-white">
               View Details
             </button>
           </Link>
           <Link href={liveLink}>
-            <button className="px-4 py-2 bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+            <button className="px-4 py-2 bg-white text-black text-xs font-bold">
               Live Link
             </button>
           </Link>
